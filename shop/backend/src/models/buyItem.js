439da8b9b061b6a5a32f778573fcb01a2e3a8f8f
Kracskó Bence <kracskobence@gmail.com>
2023-07-28 14:19:57 +0200

    .

--- shop/backend/src/models/buyItem.js
+++ shop/backend/src/models/buyItem.js
@@ -3,6 +3,6 @@ import { db } from "../data/connection";
 export const buyItem = {
     async buy(itemId){
         const response = await db.query('DELETE FROM items WHERE id = ?', [Number(itemId)]);
-        return 'ok'
+        return 'oki'
     }
 }
